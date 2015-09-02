7eec32071a8cb87896cc29a936e0a97c63dfdce6
Net <s.neige_psn@windowslive.com>
2015-09-02 16:16:22 +0700

    Create entity education

---
+++ src/main/java/com/aug/hrdb/entities/Education.java
@@ -0,0 +1,124 @@
+package com.aug.hrdb.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EDUCATION")
+public class Education extends BaseEntity{
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private Integer id;
+
+	@Column(name = "UNIVERSITY")
+	private String university;
+	
+	@Column(name = "DEGREETYPE_ID")
+	private String degree;
+
+	@Column(name = "FACULTY")
+	private String faculty;
+
+	@Column(name = "MAJOR")
+	private String major;
+
+	@Column(name = "GPA")
+	private Double gpa;
+	
+	@Column(name = "START_DATE")
+	private Date start_date;
+	
+	@Column(name = "GRADUATED_DATE")
+	private Date graduated_date;
+	
+	@Column(name = "CERTIFICATION")
+	private String certification;
+	
+/*	@ManyToOne
+	@JoinColumn(name="EMPLOYEE_ID")
+	private Applicant applicant;
+	
+	@ManyToOne
+	@JoinColumn(name="APPLICANT_ID")
+	private Applicant applicant;*/
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+	public void setFaculty(String faculty) {
+		this.faculty = faculty;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+	public Double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(Double gpa) {
+		this.gpa = gpa;
+	}
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+	public Date getGraduated_date() {
+		return graduated_date;
+	}
+
+	public void setGraduated_date(Date graduated_date) {
+		this.graduated_date = graduated_date;
+	}
+
+	public String getCertification() {
+		return certification;
+	}
+
+	public void setCertification(String certification) {
+		this.certification = certification;
+	}
+	
+}
