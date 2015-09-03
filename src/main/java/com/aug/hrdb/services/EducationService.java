b46b437979de14d8723351bf307e87eb14ba9225
Net <s.neige_psn@windowslive.com>
2015-09-03 10:02:34 +0700

    Create EducationServiceImpl and Edit EducationService

--- src/main/java/com/aug/hrdb/services/EducationService.java
+++ src/main/java/com/aug/hrdb/services/EducationService.java
@@ -1,5 +1,26 @@
 package com.aug.hrdb.services;
 
-public class EducationService {
+import java.util.List;
+
+import com.aug.hrdb.dto.EducationDto;
+import com.aug.hrdb.entities.Education;
+
+public interface EducationService {
+
+	public void create(Education education);
+
+	public Education findById(Integer id);
+
+	public void update(Education education);
+
+	public void delete(Education education);
+	
+	public void deleteByApplicantId(Integer id);
+
+	public List<Education> findAll();
+	
+	public List<EducationDto> findEducationById(Integer id);
+
+	public EducationDto findEducation(Integer id);
 
 }
---
+++ src/main/java/com/aug/hrdb/services/impl/EducationServiceImpl.java
@@ -0,0 +1,66 @@
+package com.aug.hrdb.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aug.hrdb.dto.EducationDto;
+import com.aug.hrdb.entities.Education;
+import com.aug.hrdb.repositories.EducationRepository;
+import com.aug.hrdb.services.EducationService;
+
+@Service(value = "educationService")
+@Transactional
+public class EducationServiceImpl implements EducationService{
+	
+	@Autowired
+	private EducationRepository educationRepository;
+	
+	@Override
+	public Education findById(Integer id) {
+		return educationRepository.find(id);
+	}
+
+	@Override
+	public void create(Education education) {
+		educationRepository.create(education);
+		
+	}
+
+	@Override
+	public void update(Education education) {
+		educationRepository.update(education);
+		
+	}
+
+	public void delete(Education education) {
+		educationRepository.delete(education);
+		
+	}
+	
+	@Override
+	public void deleteByApplicantId(Integer id) {
+		educationRepository.deleteByApplicantId(id);
+		
+	}
+
+	@Override
+	public List<Education> findAll() {
+		List<Education> educationList = educationRepository.findAll();
+		return educationList;
+	}
+
+	@Override
+	public List<EducationDto> findEducationById(Integer id) {
+		List<EducationDto> educations = educationRepository.findEducationById(id);
+		return educations;
+	}
+	
+	@Override
+	public EducationDto findEducation(Integer id){
+		EducationDto education = educationRepository.findByEducationId(id);
+		return education;
+	}
+}
