89d1437e08e8c6a5f13a018add143c3e806ef1a3
apivakim <nuva_apiva@outlook.co.th>
2015-09-04 11:57:24 +0700

    add method findAll in MasStaffType in Respo and unittest masstafftype service and respo complete

--- src/test/java/repositories/MasStaffTypeRepositoryTest.java
+++ src/test/java/repositories/MasStaffTypeRepositoryTest.java
@@ -1,7 +1,9 @@
 package repositories;
 
 import java.util.Calendar;
+import java.util.List;
 
+import org.hibernate.Criteria;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.aug.hrdb.entities.MasStaffType;
 import com.aug.hrdb.repositories.MasStaffTypeRepository;
+
+import junit.framework.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring-bean-db-test.xml" })
@@ -28,13 +32,63 @@ public class MasStaffTypeRepositoryTest {
 		MasStaffType masStaffType = new MasStaffType();
 		masStaffType.setName("Augmentis");
 		masStaffType.setAuditFlag("C");
-		masStaffType.setCode("STAFF-01");
+		masStaffType.setCode("STAFF-03");
 		masStaffType.setCreatedBy(1);
 		masStaffType.setCreatedTimeStamp(Calendar.getInstance().getTime());
 		masStaffType.setIsActive(true);
 
-		masStaffTypeRepository.getCurrentSession().save(masStaffType);
+		masStaffTypeRepository.create(masStaffType);
 
 	}
+	
+	
+	@Test
+	@Rollback(false)
+	public void updateMasStaffType(){
+
+		MasStaffType masStaffType =  masStaffTypeRepository.find(2);
+		masStaffType.setCode("STAFF-04");
+		masStaffType.setAuditFlag("U");
+		masStaffType.setUpdatedBy(1);
+		masStaffType.setUpdatedTimeStamp(Calendar.getInstance().getTime());
+		masStaffTypeRepository.update(masStaffType);
+		
+	}
+	
+	
+	@Test
+	@Rollback(false)
+	public void deleteMasStaffType(){
+		
+		MasStaffType masStaffType = masStaffTypeRepository.find(1);;
+		masStaffTypeRepository.delete(masStaffType);
+		
+	}
+	
+	
+	
+	@Test
+	public void listMasStaffType(){
+		
+		List<MasStaffType> masStaffTypeList = masStaffTypeRepository.findAll();
+		Assert.assertEquals(1, masStaffTypeList.size());
+		
+		for(int i=0;i<masStaffTypeList.size();i++){		
+			System.out.println("id: "+masStaffTypeList.get(i).getId());
+		}
+		
+	}
+	
+	
+	@Test
+	public void findByIdMasTechnology(){
+		
+		MasStaffType masStaffType = masStaffTypeRepository.find(3);	
+		int id = masStaffType.getId();
+		Assert.assertEquals(3, id);
+		System.out.println("id: "+id);
+		
+	}
+	
 
 }
--- src/test/java/services/MasStaffTypeServiceTest.java
+++ src/test/java/services/MasStaffTypeServiceTest.java
@@ -1,6 +1,7 @@
 package services;
 
 import java.util.Calendar;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.aug.hrdb.entities.MasStaffType;
 import com.aug.hrdb.services.MasStaffTypeService;
+
+import junit.framework.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring-bean-db-test.xml" })
@@ -29,7 +32,7 @@ public class MasStaffTypeServiceTest {
 		MasStaffType masStaffType = new MasStaffType();
 		masStaffType.setName("Augmentis");
 		masStaffType.setAuditFlag("C");
-		masStaffType.setCode("STAFF-01");
+		masStaffType.setCode("STAFF-02");
 		masStaffType.setCreatedBy(1);
 		masStaffType.setCreatedTimeStamp(Calendar.getInstance().getTime());
 		masStaffType.setIsActive(true);
@@ -38,5 +41,56 @@ public class MasStaffTypeServiceTest {
 		masStaffTypeService.create(masStaffType);
 
 	}
+	
+	
+	@Test
+	@Rollback(false)
+	public void updateMasStaffType(){
+
+		MasStaffType masStaffType = (MasStaffType) masStaffTypeService.find(1);
+		masStaffType.setCode("STAFF-05");
+		masStaffType.setAuditFlag("U");
+		masStaffType.setUpdatedBy(1);
+		masStaffType.setUpdatedTimeStamp(Calendar.getInstance().getTime());
+		masStaffTypeService.update(masStaffType);
+		
+	}
+	
+	
+	
+	@Test
+	@Rollback(false)
+	public void deleteMasStaffType(){
+		
+		MasStaffType masStaffType = masStaffTypeService.find(2);
+		masStaffTypeService.delete(masStaffType);
+		
+	}
+	
+	
+	@Test
+	public void listMasStaffType(){
+		
+		List<MasStaffType> masStaffTypeList = masStaffTypeService.findAll();
+		Assert.assertEquals(1, masStaffTypeList.size());
+		
+		for(int i=0;i<masStaffTypeList.size();i++){		
+			System.out.println("id: "+masStaffTypeList.get(i).getId());
+		}
+		
+	}
+	
+	
+	@Test
+	public void findByIdMasTechnology(){
+		
+		MasStaffType masStaffType = masStaffTypeService.find(3);	
+		int id = masStaffType.getId();
+		Assert.assertEquals(3, id);
+		System.out.println("id: "+id);
+		
+	}
+	
+	
 
 }
