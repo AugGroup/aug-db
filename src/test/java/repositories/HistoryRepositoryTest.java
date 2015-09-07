7c4817ddf032a5cf6f65d90ecf3330d1807457ab
PreeyaMay <leungsod@hotmail.com>
2015-09-07 11:23:51 +0700

    create test Repository, Service for History

---
+++ src/test/java/repositories/HistoryRepositoryTest.java
@@ -0,0 +1,77 @@
+/**
+ *
+ * @author Preeyaporn
+ * @date 7 ก.ย. 2558
+ */
+package repositories;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+import org.hibernate.Criteria;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aug.hrdb.entities.History;
+import com.aug.hrdb.repositories.HistoryRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring-bean-db-test.xml" })
+@Transactional
+public class HistoryRepositoryTest {
+	
+	@Autowired
+	private HistoryRepository historyRepository;
+
+	@Test
+	public void create() throws ParseException {
+
+		SimpleDateFormat dateFmt = new SimpleDateFormat("dd-MM-yyyy",
+				Locale.ENGLISH);
+		
+		History history = new History();
+		
+		history.setPosition("Java");
+		history.setSalary(30000d);
+		history.setDateOfAdjustment(dateFmt.parse("04-01-2015"));
+		historyRepository.getCurrentSession().save(history);
+
+	}
+
+//	@Test
+//	public void update() {
+//
+//		History history = (History) historyRepository.getCurrentSession().get(
+//				History.class, 1);
+//		history.setPosition(".Net");
+//
+//		historyRepository.getCurrentSession().update(history);
+//	}
+//
+//	@Test
+//	public void Delete() {
+//
+//		History history = (History) historyRepository.getCurrentSession().get(
+//				History.class, 1);
+//
+//		historyRepository.getCurrentSession().delete(history);
+//	}
+//
+//	@SuppressWarnings("unchecked")
+//	@Test
+//	public void list() {
+//
+//		Criteria c = historyRepository.getCurrentSession().createCriteria(
+//				History.class);
+//		List<History> histories = c.list();
+//		Assert.assertEquals(0, histories.size());
+//
+//	}
+}
---
+++ src/test/java/services/HistoryServiceTest.java
@@ -0,0 +1,71 @@
+/**
+ *
+ * @author Preeyaporn
+ * @date 7 ก.ย. 2558
+ */
+package services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aug.hrdb.entities.History;
+import com.aug.hrdb.services.HistoryService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring-bean-db-test.xml" })
+@Transactional
+public class HistoryServiceTest {
+
+	@Autowired
+	private HistoryService historyService;
+	
+	@Test
+	public void create() throws ParseException{
+
+		SimpleDateFormat dateFmt = new SimpleDateFormat("dd-MM-yyyy",
+				Locale.ENGLISH);
+		
+		History history = new History();
+		history.setPosition("Java");
+		history.setSalary(30000d);
+		history.setDateOfAdjustment(dateFmt.parse("04-01-2015"));
+		
+		historyService.create(history);
+	}
+	
+//	@Test
+//	public void update(){
+//
+//		History history = historyService.findById(2);
+//		history.setPosition(".Net");
+//		historyService.update(history);
+//		
+//	}
+//	
+//	@Test
+//	public void delete(){
+//
+//		History history = historyService.findById(2);
+//		historyService.delete(history);
+//		
+//	}
+//	
+//	
+//	@Test
+//	public void findAll(){
+//
+//		List<History> history = historyService.findAll();
+//		Assert.assertEquals(3, history.size());
+//	}
+	
+}
