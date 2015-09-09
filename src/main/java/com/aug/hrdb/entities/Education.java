40a471098a17a59660ff97f7953cdecf21afe86b
Pranrajit <pranrajit@users.noreply.github.com>
2015-09-09 17:23:45 +0700

    edit relation education and masdegreetype

--- src/main/java/com/aug/hrdb/entities/Education.java
+++ src/main/java/com/aug/hrdb/entities/Education.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -54,8 +55,8 @@ public class Education extends BaseEntity{
 	private Applicant applicant;*/
 	
 	
-	@ManyToOne()
-	@JoinColumn(name = "DEGREETYPE_ID", nullable = false)
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name = "DEGREETYPE_ID",nullable = false)
 	private MasDegreetype masdegreetype;
 	
 	public Integer getId() {
