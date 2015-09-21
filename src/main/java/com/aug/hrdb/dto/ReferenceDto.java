8423816b9f58d6c8109d39be092c9dd16378169c
Parnell <takoyakimuffin@gmail.com>
2015-09-21 14:10:19 +0700

    Edit Native query referenceDto ver2

--- src/main/java/com/aug/hrdb/dto/ReferenceDto.java
+++ src/main/java/com/aug/hrdb/dto/ReferenceDto.java
@@ -13,7 +13,7 @@ import javax.persistence.NamedNativeQuery;
             		+ "ref.occupation,"
             		+ "ref.applicant_id "
             		+ "from reference as ref "
-            		+ "where ref.applicant_id := appId", 
+            		+ "where ref.applicant_id =: appId", 
             resultClass = ReferenceDto.class),
 	@NamedNativeQuery(name = "SEARCH_REFERENCE_ID", query = "select ref.id,ref.name,"
             		+ "ref.address, ref.telephone,"
