6dc0e2649c7f089fe48a2113cb032246e547a8a6
bank <ballboad@hotmail.com>
2015-09-28 14:57:24 +0700

    add init data mailTemplate

--- src/main/resources/scripts/init-data.sql
+++ src/main/resources/scripts/init-data.sql
@@ -407,4 +407,77 @@ VALUES
 (3,'นัดเซ็นสัญญา',STR_TO_DATE('14/09/2015 11:00:00','%d/%m/%Y %H:%i:%s'),STR_TO_DATE('14/09/2015 10:00:00','%d/%m/%Y %H:%i:%s'),'นัดเซ็นสัญญาคุณ Anut',3,2),
 (4,'นัดทำข้อสอบและสอบสัมภาษณ์',STR_TO_DATE('17/09/2015 12:00:00','%d/%m/%Y %H:%i:%s'),STR_TO_DATE('17/09/2015 09:00:00','%d/%m/%Y %H:%i:%s'),'นัดทำข้อสอบคุณ Thanatchapong',4,2)
 
+--MailTemplate
+INSERT INTO MAIL_TEMPLATE VALUES(1,"Interview Java","Dear K. $firstName
 
+
+
+Thank you for interesting a Augmentis (Thailand) Limited.
+
+
+
+I would like to  confirm you about test and interview information below.
+
+
+
+Testing process $date  ($time)
+
+1.    Personality Test        10 minute
+
+2.    IQ Test                      10 minute 
+
+3.    New Grad Test          60 minute 
+
+4.    Write Application
+
+5.    Interview
+
+
+
+Document preparation
+
+1. ID card (copy)
+
+2. House registration (copy) 
+
+3. Transcript (copy)
+
+4. Photo
+
+5. Resume
+
+6. Certificates (if has)
+
+Then I attached the Augmentis map file by following.
+
+
+
+หมายเหตุ: การเดินทางโดย BTS ให้ลงสถานทีช่องนนทรี ออกทางออกที่ 5 พอถึงตึก
+
+Empire ให้ติดต่อแลกบัตรชั้นM ที่ประชาสัมพันธ์ แล้วให้ใช้ลิฟท์ Low zone
+
+(Tower2) เบอร์ 27,28,31,32 ขึ้นมาที่ชั้น 18 ค่ะ
+
+
+
+So, if you have any information and additional, please do not hesitate to
+
+contact me.
+
+
+
+Best Regards,
+
+
+
+$recruitName 
+
+$recruitPosition
+
+P : +66 2670 0930 # 112 | M : +$recruitPhone
+
+Augmentis Group <http://www.augmentis.biz/> 
+
+Bangkok | Singapore | Melbourne | Sydney | Jakarta
+
+<http://www.augmentis.biz/>  ");
