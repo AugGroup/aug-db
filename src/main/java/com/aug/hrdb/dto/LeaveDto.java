88de4cc4e69e0ad7fa5023d5f11d4dafe6a99893
apivakim <nuva_apiva@outlook.co.th>
2015-09-28 12:06:31 +0700

    fix bug leave

--- src/main/java/com/aug/hrdb/dto/LeaveDto.java
+++ src/main/java/com/aug/hrdb/dto/LeaveDto.java
@@ -21,7 +21,7 @@ import javax.persistence.TemporalType;
 @NamedNativeQueries({
 	@NamedNativeQuery(
 			name="searchLeave",
-					query="select l.id,l.aim,l.start_time,l.end_time,l.reason, m.name, l.employee_id, emp.employee_code, m.id,aim.NAME_ENG as NAME_ENG,l.STARTTIMESTRING,l.ENDTIMESTRING "
+					query="select l.id,l.aim,l.start_time,l.end_time,l.reason, m.name as NAME,l.leavetype_id as LEAVETYPE_ID, l.employee_id, emp.employee_code, m.id,aim.NAME_ENG as NAME_ENG,l.STARTTIMESTRING,l.ENDTIMESTRING "
 							+ "from LEAVES l, EMPLOYEE emp,MAS_LEAVETYPE  m , (select  e.NAME_ENG as NAME_ENG,l.ID as id from LEAVES l join EMPLOYEE e on e.ID=l.AIM where l.AIM=e.ID) aim "
 							+ "where  l.employee_id = :empId and l.employee_id = emp.id and l.leavetype_id = m.id and aim.id=l.id",																																																																					
 							resultClass = LeaveDto.class),
