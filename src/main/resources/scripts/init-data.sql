1d110ab42874b32775ab08d42151c5a42fd5e4ca
Anat <anat.pantera@gmail.com>
2015-09-29 23:10:03 +0700

    fix init-data.sql(MAIL_TEMPLATE)

--- src/main/resources/scripts/init-data.sql
+++ src/main/resources/scripts/init-data.sql
@@ -408,53 +408,79 @@ VALUES
 (4,'นัดทำข้อสอบและสอบสัมภาษณ์',STR_TO_DATE('17/09/2015 12:00:00','%d/%m/%Y %H:%i:%s'),STR_TO_DATE('17/09/2015 09:00:00','%d/%m/%Y %H:%i:%s'),'นัดทำข้อสอบคุณ Thanatchapong',4,2);
 
 --MailTemplate
-INSERT INTO MAIL_TEMPLATE VALUES(1,'JAVA',"<p>Dear K. $firstName&nbsp; &nbsp;</p>
-
-<p>Thank you for interesting a Augmentis (Thailand) Limited.&nbsp; &nbsp;</p>
-
-<p>I would like to &nbsp;confirm you about test and interview information below.&nbsp; &nbsp;</p>
-
-<p>Testing process $date &nbsp;($time)&nbsp;</p>
-
-<p>1. &nbsp;&nbsp;&nbsp;Personality Test &nbsp; &nbsp; &nbsp; &nbsp;10 minute&nbsp;</p>
-
-<p>2. &nbsp;&nbsp;&nbsp;IQ Test &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;10 minute&nbsp;&nbsp;</p>
-
-<p>3. &nbsp;&nbsp;&nbsp;New Grad Test &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;60 minute&nbsp;&nbsp;</p>
-
-<p>4. &nbsp;&nbsp;&nbsp;Write Application&nbsp;</p>
-
-<p>5. &nbsp;&nbsp;&nbsp;Interview&nbsp; &nbsp;</p>
+INSERT INTO MAIL_TEMPLATE VALUES(1,'JAVA','<p>Dear K. $FIRST_NAME</p>
 
 <p>&nbsp;</p>
 
-<p>Document preparation&nbsp;</p>
+<p>Thank you for interesting a Augmentis (Thailand) Limited.</p>
 
-<p>1. ID card (copy)&nbsp;</p>
-
-<p>2. House registration (copy)&nbsp;&nbsp;</p>
-
-<p>3. Transcript (copy)&nbsp;</p>
-
-<p>4. Photo&nbsp;</p>
-
-<p>5. Resume&nbsp;</p>
-
-<p>6. Certificates (if has)&nbsp;</p>
+<p>I would like to &nbsp;confirm you about test and interview information below.</p>
 
 <p>&nbsp;</p>
 
-<p>Then I attached the Augmentis map file by following.&nbsp; &nbsp;</p>
+<p>Testing process $DATE &nbsp;($TIME)</p>
+
+<p>1. &nbsp;&nbsp;&nbsp;Personality Test &nbsp; &nbsp; 10 minute</p>
+
+<p>2. &nbsp;&nbsp;&nbsp;IQ Test &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 10 minute&nbsp;</p>
+
+<p>3. &nbsp;&nbsp;&nbsp;New Grad Test &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;60 minute&nbsp;</p>
+
+<p>4. &nbsp;&nbsp;&nbsp;Write Application</p>
+
+<p>5. &nbsp;&nbsp;&nbsp;Interview</p>
 
 <p>&nbsp;</p>
 
-<p>หมายเหตุ: การเดินทางโดย BTS ให้ลงสถานทีช่องนนทรี ออกทางออกที่ 5 พอถึงตึก&nbsp; Empire ให้ติดต่อแลกบัตรชั้นM ที่ประชาสัมพันธ์ แล้วให้ใช้ลิฟท์ Low zone&nbsp; (Tower2) เบอร์ 27,28,31,32 ขึ้นมาที่ชั้น 18 ค่ะ&nbsp; &nbsp;</p>
+<p>Document preparation</p>
 
 <p>&nbsp;</p>
 
-<p>So, if you have any information and additional, please do not hesitate to&nbsp; contact me.&nbsp; &nbsp;</p>
+<p>1. ID card (copy)</p>
+
+<p>2. House registration (copy)&nbsp;</p>
+
+<p>3. Transcript (copy)</p>
+
+<p>4. Photo</p>
+
+<p>5. Resume</p>
+
+<p>6. Certificates (if has)</p>
 
 <p>&nbsp;</p>
 
-<p>Best Regards,&nbsp; &nbsp; $recruitName&nbsp;&nbsp; $recruitPosition&nbsp; P : +66 2670 0930 # 112 | M : +$recruitPhone&nbsp; Augmentis Group &lt;http://www.augmentis.biz/&gt;&nbsp;&nbsp; Bangkok | Singapore | Melbourne | Sydney | Jakarta&nbsp; &lt;http://www.augmentis.biz/&gt;</p>
-");
+<p>Then I attached the Augmentis map file by following.</p>
+
+<p>&nbsp;</p>
+
+<p>หมายเหตุ: การเดินทางโดย BTS ให้ลงสถานทีช่องนนทรี ออกทางออกที่ 5 พอถึงตึก</p>
+
+<p>Empire ให้ติดต่อแลกบัตรชั้นM ที่ประชาสัมพันธ์ แล้วให้ใช้ลิฟท์ Low zone</p>
+
+<p>(Tower2) เบอร์ 27,28,31,32 ขึ้นมาที่ชั้น 18 ค่ะ</p>
+
+<p>&nbsp;</p>
+
+<p>So, if you have any information and additional, please do not hesitate to</p>
+
+<p>contact me.</p>
+
+<p>&nbsp;</p>
+
+<p>Best Regards,</p>
+
+<p>&nbsp;</p>
+
+<p>$RECRUIT_FIRST_NAME $RECRUIT_LAST_NAME&nbsp;</p>
+
+<p>$RECRUIT_POSITION</p>
+
+<p>P : +66 2670 0930 # 112 | M : $RECRUIT_PHONE</p>
+
+<p>Augmentis Group &lt;<a href="http://www.augmentis.biz/">http://www.augmentis.biz/</a>&gt;&nbsp;</p>
+
+<p>Bangkok | Singapore | Melbourne | Sydney | Jakarta</p>
+
+<p>&lt;<a href="http://www.augmentis.biz/">http://www.augmentis.biz/</a>&gt;</p>
+');
