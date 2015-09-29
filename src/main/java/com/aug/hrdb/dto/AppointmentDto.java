ba139db346baf1438284778adf1833140bd104ae
PiyauwtK <kan.piyawut@gmail.com>
2015-09-29 14:59:55 +0700

    edit AppointmentDTO

--- src/main/java/com/aug/hrdb/dto/AppointmentDto.java
+++ src/main/java/com/aug/hrdb/dto/AppointmentDto.java
@@ -29,11 +29,15 @@ import com.sun.mail.handlers.image_gif;
 					"WHERE DATE(APPOINTMENT.START) >=STR_TO_DATE(:START,'%Y-%m-%d') AND  DATE(APPOINTMENT.END) <= STR_TO_DATE(:END,'%Y-%m-%d')", resultClass = AppointmentDto.class),
 	
 	@NamedNativeQuery(name = "GET_APPOINTMENT_BY_ID", 
-			query = "SELECT APPOINTMENT.ID , APPOINTMENT.DETAIL , APPOINTMENT.END , APPOINTMENT.START ,APPOINTMENT.TOPIC ,APPOINTMENT.APPLICANT_ID ,APPOINTMENT.LOGIN_ID  " +
-					"FROM APPOINTMENT " +
-					"LEFT JOIN APPLICANT  on APPOINTMENT.APPLICANT_ID = APPLICANT.ID " + 
-					"LEFT JOIN  LOGIN  on APPOINTMENT.LOGIN_ID = LOGIN.ID " +
-					"WHERE APPOINTMENT.ID = :ID", resultClass = AppointmentDto.class)
+			query = "SELECT appo.ID , appo.DETAIL , appo.END , appo.START ,appo.TOPIC ,appo.APPLICANT_ID ,appo.LOGIN_ID, " +
+					"CONCAT(appl.FIRSTNAME_EN,' ',appl.LASTNAME_EN) as APPLICANT_NAME, "+
+					"CONCAT(mt.NAME,' ',mj.NAME) as APPLICANT_POSITION "+
+					"FROM APPOINTMENT appo " +
+					"LEFT JOIN APPLICANT appl on appo.APPLICANT_ID = appl.ID " + 
+					"LEFT JOIN MAS_TECHNOLOGY mt ON appl.MASTECHNOLOGY_ID = mt.ID "+
+					"LEFT JOIN MAS_JOBLEVEL mj ON appl.MASJOBLEVEL_ID = mj.ID "+
+					"LEFT JOIN  LOGIN  lo on appo.LOGIN_ID = lo.ID " +
+					"WHERE appo.ID = :ID", resultClass = AppointmentDto.class)
 })
 
 public class AppointmentDto {
@@ -48,12 +52,12 @@ public class AppointmentDto {
 	@Column(name = "DETAIL")
 	private String detail;
 	
-	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MM/yyyy HH:mm:ss")
 	@Column(name = "START")
   //  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date start;
 	
-	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MM/yyyy HH:mm:ss")
 	@Column(name = "END")
   //  @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date end;
@@ -64,6 +68,12 @@ public class AppointmentDto {
 	@Column(name = "LOGIN_ID")
 	private Integer loginId;
 
+	@Column(name = "APPLICANT_NAME")
+	private String applicantName;
+	
+	@Column(name = "APPLICANT_POSITION")
+	private String applicantPosition;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -118,6 +128,22 @@ public class AppointmentDto {
 
 	public void setLoginId(Integer loginId) {
 		this.loginId = loginId;
+	}
+
+	public String getApplicantName() {
+		return applicantName;
+	}
+
+	public void setApplicantName(String applicantName) {
+		this.applicantName = applicantName;
+	}
+
+	public String getApplicantPosition() {
+		return applicantPosition;
+	}
+
+	public void setApplicantPosition(String applicantPosition) {
+		this.applicantPosition = applicantPosition;
 	}
 	
 	
