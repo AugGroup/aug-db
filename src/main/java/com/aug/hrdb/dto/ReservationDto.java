21b22e91223bffabcc17623ed1e7468d0987626a
bill <bisbifltl@gmail.com>
2015-11-02 10:04:40 +0700

    edit query reservation

--- src/main/java/com/aug/hrdb/dto/ReservationDto.java
+++ src/main/java/com/aug/hrdb/dto/ReservationDto.java
@@ -25,7 +25,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 	@NamedNativeQuery(name="GET_RESERVATIONS",
 		query = "SELECT ID,START_TIME,END_TIME,DESCRIPTION as TITLE, DESCRIPTION, "+
 				"null as RELATION_NAME, null as DIVISION_NAME, null as DATE_RESERVATION, null as RESERVATION_TYPE, "+
-				"null as ROOM_NAME ,null as RESERVED_BY FROM RESERVATION "+
+				"null as ROOM_NAME ,null as RESERVATION_BY FROM RESERVATION "+
 	            "r WHERE DATE(r.`START_TIME`) >= STR_TO_DATE(:START,'%Y-%m-%d') "+
 				"AND  DATE(r.`END_TIME`) <= STR_TO_DATE(:END,'%Y-%m-%d')",
 		resultClass = ReservationDto.class),
@@ -33,11 +33,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 	@NamedNativeQuery(name="GET_RESERVATION_ID",
 		query = "SELECT r.ID, START_TIME, END_TIME, DATE_RESERVATION, r.DESCRIPTION as TITLE, "+
 				"ro.NAME as ROOM_NAME, mr.NAME as RESERVATION_TYPE, md.NAME as DIVISION_NAME, "+
-				"r.DESCRIPTION,CONCAT(app.FIRSTNAME_EN,' ',app.LASTNAME_EN) as RESERVED_BY FROM RESERVATION r "+
+				"r.DESCRIPTION, RESERVATION_BY FROM RESERVATION r "+
 				"LEFT JOIN MAS_RESERVATION_TYPE mr ON r.RESERVATION_TYPE_ID = mr.ID "+
 				"LEFT JOIN ROOM ro ON r.ROOM_ID = ro.ID "+
 				"LEFT JOIN EMPLOYEE e ON r.EMPLOYEE_ID = e.ID "+
-				"LEFT JOIN MAS_DIVISION md ON e.DIVISION_ID = md.ID "+
+				"LEFT JOIN MAS_DIVISION md ON r.DIVISION_ID = md.ID "+
 				"LEFT JOIN APPLICANT app ON e.APPLICANT_ID = app.ID "+
 				"WHERE r.ID = :SEARCH_ID ",
 		resultClass = ReservationDto.class)			
@@ -83,7 +83,7 @@ public class ReservationDto {
 	@Column(name="DESCRIPTION")
 	private String description;
 	
-	@Column(name="RESERVED_BY")
+	@Column(name="RESERVATION_BY")
 	private String reservedBy;
 
 	public Integer getId() {
