bbe5423847e5d56f14320710dc62f22e082befa6
Pranrajit <pranrajit@users.noreply.github.com>
2015-11-03 09:17:36 +0700

    edit config

--- src/main/java/com/aug/hrdb/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/aug/hrdb/services/impl/ReservationServiceImpl.java
@@ -69,14 +69,14 @@ public class ReservationServiceImpl implements ReservationService {
 	}
 
 	@Override
-<<<<<<< HEAD
+
 	public List<ReservationDto> searchReservation(Reservation reservation) {
 		// TODO Auto-generated method stub
 		return reservationRepository.searchReservation(reservation);
-=======
+
 	public List<ReservationDto> findByTimestamp(String newTime) {
 		// TODO Auto-generated method stub
 		return reservationRepository.findByTimestamp(newTime);
->>>>>>> query GET_RESERVATION_BY_TIMESTAMP service and repo
+
 	}
 }
